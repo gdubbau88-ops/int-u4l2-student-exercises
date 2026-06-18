b947927518b3cbfb18fdbb6424a3523248206e3a
Gavin Mitchell <gavin.mitchell@ntithunderbots.com>
2026-06-18 11:35:34 -0500

    made form work using concatenation.

--- concatenation.js
+++ concatenation.js
@@ -7,13 +7,15 @@ submitButton.addEventListener("click", function() {
     let mood = document.querySelector(".mood").value;
     let food = document.querySelector(".food-name").value;
     let birthYear = document.querySelector(".year").value;
-    let age = 2023 - birthYear; // update to 2024, if necessary
+    let age = 2026 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+    let city=document.querySelector(".city").value;
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
-
+// results.innerHTML = "city" + " " + city + " ";
+// results.innerHTML += "is Where I live!"
+    results.innerHTML="My name is" + " "+ (myName) + "."  +" " + "I'm" + " "+ (age)+" " +  "years old." +" " + "I'm from" + " " + (city) + "," + " " +  "and I'm very" + " " + (mood) + " " +  "I love to eat" + " " + (food);
 
 });
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let buttonOne = document.querySelector(".button-one");
 let captionOne = document.querySelector(".caption-one");
-
+let word1 = "butter";
+let word2 = "fly";
 buttonOne.addEventListener("click", function() {
     // 1. Declare a variable named spidyCaption.
     //  - Store the value of the input with a class of "comment-one".
-
+let spidyCaptionHTMLElement=document.querySelector(".comment-one");
+    let spidyCaption= spidyCaptionHTMLElement.value;
 
     // 2. Display the variable spidyCaption to captionOne.
-
-
+captionOne.innerHTML = spidyCaption;
+console.log(word1 + word2);
 });
 
 // 3. Repeat the previous steps for the second meme.
